f808b5674eb7f37f103840ecc8e9ffd2f9610e68
Gerald Nunn <gnunn1@users.noreply.github.com>
2018-07-19 13:55:09 -0400

    Update nav.jsx

--- react-crud/libs/js/react/components/layouts/nav.jsx
+++ react-crud/libs/js/react/components/layouts/nav.jsx
@@ -45,14 +45,13 @@ var NavComponent = React.createClass({
             <div>
                 <nav className="navbar navbar-default navbar-fixed-top">
                     {/* Remove comment block below to show change  */}
-                    {/*
+                    
                     <div className="navbar-header">
                         <a className="navbar-brand" href="http://github.com/gnunn1/openshift-basic-pipeline">
                             <img src="http://icons.iconarchive.com/icons/graphicloads/100-flat/24/phone-icon.png" alt="Best Electronics"/><span>  Best Electronics</span>
                         </a>
                     </div>
-                    */}
-                    
+                                        
                                         
                     <div className="container">
                         {
